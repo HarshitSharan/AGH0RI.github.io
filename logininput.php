a23cb7ac5ac099b5e8c6858be88d5047ab850d97
AGH0RI <59949856+AGH0RI@users.noreply.github.com>
2020-01-16 12:29:33 +0530

    uploading

---
+++ logininput.php
@@ -0,0 +1,16 @@
+<form action="login.php" method="POST">
+ <table border="2" cellpadding="20" cellspacing="10" align="center">
+ 	<tr>
+ 		<td>UserName</td>
+ 		<td><input type="text" name="UserName" placeholder="Enter your user name"></td>
+ 	</tr>
+ 	<tr>
+ 		<td>Password</td>
+ 		<td><input type="password" name="pass" placeholder="*********"></td>
+ 	</tr>
+ 	<tr>
+ 		<td colspan="2" align="center"><a href="signup.html"><button type="button"> SIGNUP </button></a> <button type="submit"> LOGIN </button></td>
+
+ 	</tr>
+ </table>
+</form>
